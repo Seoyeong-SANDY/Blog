a900d60a62a53a01e8df324c5ed5e76f783f7f41
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-03-22 22:45:31 +0900

    .

--- blog-config.js
+++ blog-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: "제이티의 기술 블로그",
-  description: "안녕하세요! 모바일 앱 개발자를 꿈꾸는 JT입니다!",
-  author: "SY",
+  title: "SANDY의 기술 블로그",
+  description: "안녕하세요! 게임 클라이언트 개발자를 꿈꾸는 SANDY입니다!",
+  author: "SANDY",
   siteUrl: "https://seoyeong-sandy.netlify.app",
   links: {
     // github: "https://github.com",
@@ -9,6 +9,7 @@ module.exports = {
     // facebook: "https://www.facebook.com",
     // instagram: "https://www.instagram.com",
     // email: "mailto:dokkan43@gmail.com",
+    youtube: "https://www.naver.com"
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
