6afddba11af52e3ee5986caf3704c045a0d78a4a
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-03-22 22:41:47 +0900

    Update siteUrl

--- blog-config.js
+++ blog-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: "제이티의 기술 블로그",
   description: "안녕하세요! 모바일 앱 개발자를 꿈꾸는 JT입니다!",
-  author: "JT",
-  siteUrl: "https://jt-tech-blog.netlify.app/",
+  author: "SY",
+  siteUrl: "https://seoyeong-sandy.netlify.app",
   links: {
     // github: "https://github.com",
     // linkedIn: "https://linkedin.com",
